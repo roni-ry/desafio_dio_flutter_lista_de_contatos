34da3fd661d8c9280be922e3f2bd43e7f4ee266b
roni <ronald.dev@gmail.com>
2023-10-22 15:40:35 -0300

    Criando um App Flutter de Lista de Contatos

---
+++ README.md
@@ -0,0 +1,3 @@
+# desafio_dio_lista_de_contatos
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/desafio_dio_lista_de_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.desafio_dio_lista_de_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
